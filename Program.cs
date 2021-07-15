1d9837cc26b99d8fa95a7ff96dd37deee60304f2
Andy Thackray <thackrayaf@gmail.com>
2021-07-15 17:43:23 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace blazor_web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+
+    public class Checkout
+  {
+    public string Product { get; set; }
+    public string Email { get; set; }
+    public string Country { get; set; }
+    public string Postcode { get; set; }
+    public string Override { get; set; }
+  }
+}
+
